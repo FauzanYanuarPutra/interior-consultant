b7f7180631cefefa54685f9d08555235d883511f
Fauzanyan <ccdplayerx@gmail.com>
2023-06-30 14:37:00 +0700

    add project

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,75 @@
 <template>
-<div class="flex justify-between items-center relative mt-5 p-5 md:px-20 w-full  z-20">
-  <div class="">
-    <div class="px-2 py-1 border-[1px] border-white text-white judul text-sm" v-show="!isOpen">THIS INTERIOR</div>
+<div>
+  <div class="flex justify-between items-center relative mt-5 p-5  w-full  z-20">
+    <div class="">
+      <div class="px-2 py-1 border-[1px] border-white text-white judul text-sm" v-show="!isOpen">THIS INTERIOR</div>
+    </div>
+    <div class=" md:hidden toggle-wrap  flex justify-end  z-20 " @click="toggleSidebar" :class="{ active: isOpen }">
+      <span class="toggle-bar "></span>
+    </div>
+    <div class="md:flex hidden text-white">
+      <a href="https://muhammadfauzan.netlify.app/" class="font-bold  md:mx-5 lg:mx-8 text-[18px] mont">
+        <span class=" inline-block border-b-[2px] pb-1">Home</span>
+      </a>
+      <a href="https://muhammadfauzan.netlify.app/" class=" md:mx-5 lg:mx-8 text-[18px] mont">Collection</a>
+      <a href="https://muhammadfauzan.netlify.app/" class=" md:mx-5 lg:mx-8 text-[18px] mont">About</a>
+      <a href="https://muhammadfauzan.netlify.app/contact" class=" md:mx-5 lg:mx-8 text-[18px] mont">Contact</a>
+    </div>
   </div>
-  <div class=" md:hidden toggle-wrap  flex justify-end  z-20 " @click="toggleSidebar" :class="{ active: isOpen }">
-    <span class="toggle-bar "></span>
-  </div>
-  <div class="md:flex hidden text-white">
-    <h1 class="font-bold  md:mx-5 lg:mx-8 text-[18px] mont">
-      <span class=" inline-block border-b-[2px] pb-1">Home</span>
-    </h1>
-    <h1 class=" md:mx-5 lg:mx-8 text-[18px] mont">Collection</h1>
-    <h1 class=" md:mx-5 lg:mx-8 text-[18px] mont">About</h1>
-    <h1 class=" md:mx-5 lg:mx-8 text-[18px] mont">Contact</h1>
-  </div>
-</div>
 
-<div id="content" class="pt-[30px] mx-5 text-white md:flex md:mx-20  justify-between items-center">
-  <div class="md:w-[35%]  pr-4">
-    <h1 class=" text-[36px] md:text-[48px] font-[500] lora ">Modern interior</h1>
-    <p class="my-5 mont font-[400] ">A full-Service redential & commercial interior design and staging company offering professional organizing & echo-services.</p>
-    <a href="" class="font-bold mont flex items-center">Read more 
-      <svg class="ml-2 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g id="arrow-right"> <g> <polyline data-name="Right" fill="none" id="Right-2" points="16.4 7 21.5 12 16.4 17" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline> <line fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="2.5" x2="19.2" y1="12" y2="12"></line> </g> </g> </g> </g></svg>
-    </a>
-  </div>
-  <div class=" flex justify-center md:justify-end">
-    <div class="relative my-10 w-[391px] md:w-[400px] lg:w-[600px]  ">
-      <img :src="'photo1.png'" alt="" class="w-full">
-      <div class="absolute bottom-[-60px] box-shadow right-5 px-5 py-3 bg-[#181719]">
-        <div class="flex">
-          <div class="pr-5"><img :src="'photo2.png'" alt="" class="w-10 rounded-full "></div>
-          <div>
-            <h1 class="mont font-medium text-sm">Aliza Webber</h1>
-            <p class="mont text-[12px] mt-[1px] text-[#828282]">Interior designer</p>
+  <div id="content" :class="{ 'slide-open': isOpen }" class="pt-[30px] mx-5 text-white md:flex md:mx-20  justify-between items-center z-20">
+    <div class="md:w-[35%]  pr-4">
+      <h1 class=" text-[36px] md:text-[48px] font-[500] lora ">Modern interior</h1>
+      <p class="my-5 mont font-[400] ">A full-Service redential & commercial interior design and staging company offering professional organizing & echo-services.</p>
+      <a href="https://muhammadfauzan.netlify.app/" class="font-bold mont flex items-center">Read more
+        <svg class="ml-2 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            <title></title>
+            <g id="Complete">
+              <g id="arrow-right">
+                <g>
+                  <polyline data-name="Right" fill="none" id="Right-2" points="16.4 7 21.5 12 16.4 17" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline>
+                  <line fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="2.5" x2="19.2" y1="12" y2="12"></line>
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </a>
+    </div>
+    <div class=" flex justify-center md:justify-end">
+      <div class="relative my-10 w-[391px] md:w-[400px] lg:w-[600px]  ">
+        <img :src="'photo1.png'" alt="" class="w-full ">
+        <div class="absolute bottom-[-60px] box-shadow right-1  sm:right-2 md:right-3 px-5 py-3 bg-[#181719]">
+          <div class="flex">
+            <div class="pr-5"><img :src="'photo2.png'" alt="" class="w-10 rounded-full "></div>
+            <div>
+              <h1 class="mont font-medium text-sm">Aliza Webber</h1>
+              <p class="mont text-[12px] mt-[1px] text-[#828282]">Interior designer</p>
+            </div>
           </div>
-        </div>
-        <div class="my-5 w-[70%] font-bold lora text-[18px] lg:text-[24px]">
-          Designed in 2020 by Aliza Webber
+          <div class="my-5 w-[70%] font-bold lora text-[18px] lg:text-[24px]">
+            Designed in 2020 by Aliza Webber
+          </div>
         </div>
       </div>
     </div>
   </div>
+
+  <h1 class="mt-[100px] py-10 flex justify-center text-white">Created By <a href="https://muhammadfauzan.netlify.app/" class="underline mx-2">Fauzan</a>- devChallenges.io</h1>
+
 </div>
-
-<h1 class="mt-[100px] py-10 flex justify-center text-white">Created By <a href="" class="underline mx-2">Fauzan</a>- devChallenges.io</h1>
-
-<transition name="slide">
-  <div v-if="isOpen" class="sidebar text-center text-white fixed  top-0 left-0 bottom-0 right-0 bg-[#181719] overflow-y-auto flex justify-center items-center" @click="toggleSidebar">
+<transition name="slide" >
+  <div v-if="isOpen" class="sidebar text-center text-white fixed  top-0 left-0 bottom-0 right-0 bg-[#181719] overflow-y-auto flex justify-center items-center">
     <div>
-      <h1 class="font-bold my-4 text-[18px]">
+      <a href="https://muhammadfauzan.netlify.app/" class="block font-bold my-4 text-[18px]">
         <span class=" inline-block border-b-[2px] pb-1">Home</span>
-      </h1>
-      <h1 class="my-4 text-[18px] mont">Collection</h1>
-      <h1 class="my-4 text-[18px] mont">About</h1>
-      <h1 class="my-4 text-[18px] mont">Contact</h1>
+      </a>
+      <a href="https://muhammadfauzan.netlify.app/" class="block my-4 text-[18px] mont">Collection</a>
+      <a href="https://muhammadfauzan.netlify.app/" class="block my-4 text-[18px] mont">About</a>
+      <a href="https://muhammadfauzan.netlify.app/contact" class="block my-4 text-[18px] mont">Contact</a>
     </div>
   </div>
 </transition>
@@ -87,13 +103,11 @@ export default {
     function disableScroll() {
       document.documentElement.style.overflow = 'hidden';
       document.body.style.overflow = 'hidden';
-      document.body.style.position = 'fixed';
     }
 
     function enableScroll() {
       document.documentElement.style.overflow = 'auto';
       document.body.style.overflow = 'auto';
-      document.body.style.position = 'static';
     }
 
     function handleResize() {
@@ -143,12 +157,12 @@ export default {
 <style>
 .judul {
   font-family: 'Crimson Pro',
-serif;
+    serif;
 }
 
 .mont {
   font-family: 'Montserrat',
-sans-serif;
+    sans-serif;
 }
 
 .lora {
@@ -213,6 +227,11 @@ body {
 
 .toggle-bar::after {
   top: 5px;
+}
+
+.slide-open #content {
+  transform: translateX(250px);
+  /* Adjust the value to match the sidebar width */
 }
 
 .toggle-wrap.active .toggle-bar {
